c550699267c50d7629bb095e8e20d7d43e00756e
Gustavo Emanuel Farias Rosa <gustavo.emanuel01@outlook.com>
2021-12-24 19:49:27 -0300

    typeorm

---
+++ src/conect_typorm.ts
@@ -0,0 +1,13 @@
+import "reflect-metadata"
+
+
+import { createConnection, Connection } from "typeorm";
+
+createConnection({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "e",
+    password: "e",
+    database: "e"
+});
--- src/serve.ts
+++ src/serve.ts
@@ -1,11 +1,27 @@
-import express from 'express';
+import './conect_typorm'
+
+
+import * as express from 'express';
+import { getRepository } from 'typeorm';
+import { User } from './entity/User';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('ab aaaas')
-    return res.send('hw  12w')
+app.use(express.json())
 
+app.get('/', async (req, res) => {
+    //const repoUser = getRepository(User)
+    //let user = new User();
+    //.first_name = 'Gustavo'
+    //user.last_name = 'Emanuel'
+    //user.age = 11
+
+    //let user_saved = await repoUser.save(user)
+
+
+    //return res.send(user_saved)
+
+    return res.send('s')
 })
 
 
